77d7075eccca6e082167f8795c51edec96dd69e1
Jonathan Schaffer <jonizzyschaffer@gmail.com>
2019-05-28 11:20:30 -0500

    commiting plants list

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace planets_spaceship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> last = new List<string>();
+            last.Add("Uranus");
+            last.Add("Neptune");
+            planetList.AddRange(last);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            planetList.Remove("Pluto");
+
+            foreach(string planet in planetList) {
+                Console.WriteLine(planet);
+            }
+
+            Console.WriteLine("---------");
+            foreach(string rock in rockyPlanets) {
+                Console.WriteLine(rock);
+            }
+
+
+        }
+    }
+}
